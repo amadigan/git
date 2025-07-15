f40d69e4e58fa415179c967db318e8266afc507e
Andrew Madigan <amadigan@users.noreply.github.com>
2025-07-14 20:04:03 -0700

    update to git v2.50.1

--- build-props.sh
+++ build-props.sh
@@ -1,4 +1,4 @@
-GIT_VERSION=8c9ea59d6eeabbfc5642d99353fce2192645ba1c
+GIT_VERSION=f92a7aa85a6c7fe9c45d944ec3b37598ede47274
 GIT_REPO="https://git.kernel.org/pub/scm/git/git.git"
 MACOS_VERSION="13"
 BUILD_TAR=1
